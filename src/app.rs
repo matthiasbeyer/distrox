5062a7541a3c8263aeb3f5ab32978d48d27e75bc
Matthias Beyer <mail@beyermatthias.de>
2020-04-28 21:23:23 +0200

    Add App::new_post() method
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeMap;
 use failure::Error;
 
 use crate::types::util::IPFSHash;
+use crate::types::util::MimeType;
 use crate::types::util::IPNSHash;
 use crate::types::block::Block;
 use crate::repository::repository::Repository;
@@ -37,6 +38,34 @@ impl App {
         let device_name  = repo.publish(&publishing_key, &head).await?;
 
         Ok(App { repo, device_name, publishing_key: publishing_key.to_string() })
+    }
+
+    pub async fn new_post(&self,
+                          content: Vec<u8>,
+                          mime: MimeType,
+                          reply_to: Option<IPFSHash>,
+                          comments_will_be_propagated: Option<bool>,
+                          comments_propagated_until: Option<Timestamp>)
+        -> Result<(), Error>
+    {
+        let content_hash = self.repo.put_raw_bytes(content).await?;
+
+        let payload = Payload::Post {
+            content_format: mime,
+            content: content_hash,
+            reply_to,
+            comments_will_be_propagated,
+            comments_propagated_until,
+        };
+        let timestamp = Timestamp::now();
+        let content = Content::new(vec![], Some(timestamp), payload);
+
+        let content_hash = self.repo.put_content(content).await?;
+
+        let head         = self.repo.put_block(Block::new(protocol_version(), vec![], content_hash)).await?;
+        let device_name  = self.repo.publish(&self.publishing_key, &head).await?;
+
+        Ok(())
     }
 
 
