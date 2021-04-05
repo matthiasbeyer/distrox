79a83f1ffc4fd328e10f4fcdfddb9d5d3682601a
Matthias Beyer <mail@beyermatthias.de>
2021-04-05 13:12:36 +0200

    Add cache member in profile
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/profile.rs
+++ src/profile.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 use crate::backend::Id;
@@ -6,18 +8,22 @@ use crate::backend::IpfsEmbedBackend;
 
 pub struct Profile {
     dag: daglib::AsyncDag<Id, Node, IpfsEmbedBackend>,
+
+    cache: HashMap<cid::Cid, LoadedNode>,
 }
 
 impl Profile {
     pub async fn load(head: Id) -> Result<Self> {
         let backend = IpfsEmbedBackend::new_in_memory(1000).await?;
         let dag = daglib::AsyncDag::load(backend, head).await?;
-        Ok(Profile { dag })
+        let cache = HashMap::new();
+        Ok(Profile { dag, cache })
     }
 
     pub async fn create(node: Node) -> Result<Self> {
         let backend = IpfsEmbedBackend::new_in_memory(1000).await?;
         let dag = daglib::AsyncDag::new(backend, node).await?;
-        Ok(Profile { dag })
+        let cache = HashMap::new();
+        Ok(Profile { dag, cache })
     }
 }
