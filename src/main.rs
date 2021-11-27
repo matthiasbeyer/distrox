bec31f130dcc8bc0eae5f04ba14320d92412392d
Matthias Beyer <mail@beyermatthias.de>
2021-11-27 10:58:03 +0100

    Remove unused imports
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
-use std::path::PathBuf;
-use std::str::FromStr;
-
-use anyhow::anyhow;
 use anyhow::Result;
-use daglib::DagBackend;
-use rand_os::OsRng;
-use rand_core::CryptoRng;
-use rand_core::RngCore;
-use ed25519_dalek::Keypair;
-use ed25519_dalek::Signature;
-use futures::stream::StreamExt;
 
 extern crate clap_v3 as clap;
 
