f1fb13130d471912401d91b284456f325701f6f5
Matthias Beyer <mail@beyermatthias.de>
2021-12-19 23:07:48 +0100

    Make GossipMessage::into_bytes() pub in module
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- lib/src/gossip/msg.rs
+++ lib/src/gossip/msg.rs
@@ -9,7 +9,7 @@ pub enum GossipMessage {
 }
 
 impl GossipMessage {
-    pub(super) fn into_bytes(self) -> Result<Vec<u8>> {
+    pub(crate) fn into_bytes(self) -> Result<Vec<u8>> {
         serde_json::to_string(&self)
             .map(String::into_bytes)
             .map_err(anyhow::Error::from)
