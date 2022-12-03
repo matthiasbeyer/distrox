a9ac5f744fd8ec011b941b6db91e0ea63c1f4335
Matthias Beyer <mail@beyermatthias.de>
2022-12-03 20:53:32 +0100

    Remove API mocking
    As the devtools in tauri now work on my machine, we don't need this
    anymore.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- gui/frontend/public/glue.js
+++ gui/frontend/public/glue.js
@@ -1,30 +1,11 @@
-var fn_login = async function (name) {
-    console.log("invoke login(${name}");
-}
-
-var fn_create_account = async function (name) {
-    console.log("invoke create_account(${name}");
-}
-
-if (typeof(window.__TAURI__) !== 'undefined') {
-    const invoke = window.__TAURI__.invoke
-
-    fn_login = async function (name) {
-        return await invoke("login", {name: name});
-    }
-
-    fn_create_account = async function (name) {
-        return await invoke("create_account", {name: name});
-    }
-}
-
+const invoke = window.__TAURI_INVOKE__
 
 export async function invokeLogin(name) {
-    return await fn_login(name);
+    return await invoke("login", {name: name});
 }
 
 export async function invokeCreateAccount(name) {
-    return await fn_create_account(name);
+    return await invoke("create_account", {name: name});
 }
 
 
