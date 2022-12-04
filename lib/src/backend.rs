93138ad8f415aeff014478f947fcbd17fb439f8b
Matthias Beyer <mail@beyermatthias.de>
2022-12-04 14:43:36 +0100

    Add "Backend" trait
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ lib/src/backend.rs
@@ -0,0 +1,10 @@
+use cid::Cid;
+use libipld::Ipld;
+
+#[async_trait::async_trait]
+pub trait Backend {
+    type Error;
+
+    async fn put(&self, dag: Ipld) -> Result<(), Self::Error>;
+    async fn get(&self, cid: Cid) -> Result<Ipld, Self::Error>;
+}
