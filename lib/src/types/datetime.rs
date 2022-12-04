b18e21165ccbff158fc028e1cdf9039395672582
Matthias Beyer <mail@beyermatthias.de>
2022-12-04 10:26:03 +0100

    Replace chrono with time
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- lib/src/types/datetime.rs
+++ lib/src/types/datetime.rs
@@ -3,34 +3,39 @@ use crate::error::Error;
 use crate::types::{FromIPLD, IntoIPLD};
 
 #[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub struct DateTime(chrono::DateTime<chrono::Utc>);
+pub struct DateTime(time::OffsetDateTime);
 
 impl IntoIPLD for DateTime {
     fn into_ipld(self) -> libipld::Ipld {
-        libipld::Ipld::String(self.0.to_rfc3339())
+        libipld::Ipld::String(
+            self.0
+                .format(&time::format_description::well_known::Rfc3339)
+                .unwrap(),
+        )
     }
 }
 
 impl FromIPLD for DateTime {
     fn from_ipld(ipld: &libipld::Ipld) -> Result<DateTime, Error> {
         match ipld {
-            libipld::Ipld::String(ref s) => chrono::DateTime::parse_from_rfc3339(&s)
-                .map(|dt| dt.with_timezone(&chrono::Utc))
-                .map(DateTime)
-                .map_err(Error::from),
+            libipld::Ipld::String(ref s) => {
+                time::OffsetDateTime::parse(&s, &time::format_description::well_known::Rfc3339)
+                    .map(DateTime)
+                    .map_err(Error::from)
+            }
             _ => Err(Error::ExpectedStringForTimestamp),
         }
     }
 }
 
-impl From<chrono::DateTime<chrono::Utc>> for DateTime {
-    fn from(dt: chrono::DateTime<chrono::Utc>) -> Self {
+impl From<time::OffsetDateTime> for DateTime {
+    fn from(dt: time::OffsetDateTime) -> Self {
         DateTime(dt)
     }
 }
 
 impl DateTime {
-    pub fn inner(&self) -> &chrono::DateTime<chrono::Utc> {
+    pub fn inner(&self) -> &time::OffsetDateTime {
         &self.0
     }
 }
