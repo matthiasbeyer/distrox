bc05cac5953da0778e47f4be098a5ef1cf471e61
Matthias Beyer <mail@beyermatthias.de>
2023-08-05 16:02:50 +0200

    Add installation of callbacks
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- distrox-gui/src/lib.rs
+++ distrox-gui/src/lib.rs
@@ -1,9 +1,19 @@
 slint::include_modules!();
 
 pub mod error;
+
 use crate::error::Error;
 
 pub fn start() -> Result<(), Error> {
     let ui = AppWindow::new()?;
+    install_callbacks(&ui)?;
     ui.run().map_err(Error::from)
+}
+
+fn install_callbacks(ui: &AppWindow) -> Result<(), Error> {
+    ui.on_post_text_content(move |text| {
+        println!("{}", text);
+    });
+
+    Ok(())
 }
